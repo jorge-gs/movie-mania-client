90e3966690f04651dd671e0eb288bf7850278735
jorge-gs <jorgeluis.garcia@ujcv.edu.hn>
2017-03-18 16:39:57 -0600

    Commit Inicial

---
+++ app/src/main/java/com/example/universidad/moviemania/PeliculasActivity.java
@@ -0,0 +1,13 @@
+package com.example.universidad.moviemania;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PeliculasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peliculas);
+    }
+}
